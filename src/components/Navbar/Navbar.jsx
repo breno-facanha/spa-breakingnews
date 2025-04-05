12308f614c7564ce5cdb931641eaf679099f1ea2
Breno Façanha <brenofacanha.ti@gmail.com>
2025-04-05 15:23:24 -0300

    criado o componente Navbar e estilizado com tailwindCss

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import logo from "../../images/LogoBN.png"
+import { IoSearchOutline } from "react-icons/io5";
+
+
+export default function Navbar(){
+    return (
+        <>
+            <nav className="flex justify-between items-center h-28 px-4  border-b-1 border-gray-400">
+                <div className="flex justify-end items-center relative">
+                    <i className="absolute text-[#AAAAAA] bg-[#F1F1F1] mr-2"> <IoSearchOutline size={23}/></i>
+                    <input 
+                        placeholder="Pesquise por um título"
+                        type="text" 
+                        className="w-56 h-10 p-3 
+                        text-[#AAAAAA] bg-[#F1F1F1]
+                        border-1 outline-none border-[#F1F1F1] 
+                        rounded-md focus:border-[#0bade3] " />
+                </div>
+
+                <img src={logo} alt="Imagen Logo Breaking News" className="cursor-pointer w-[145px] h-[145px]" />
+
+                <button 
+                    className="cursor-pointer w-34 h-12 rounded-md font-medium tracking-widest
+                    bg-[#2C8AB4] text-white  hover:bg-[#0a86af]
+                ">
+                    ENTRAR
+                </button>
+            </nav>
+        </>
+    )
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+export default function(){
+    return (
+        <div className="">
+            <Navbar />
+            <div className="bg-[#F1F1F1] h-30">Pagina Home</div>
+        </div>
+    )
+}
