bc59487c5648d118f0e4c32f5873ed112f16343a
Breno Façanha <brenofacanha.ti@gmail.com>
2025-04-08 01:02:20 -0300

    consumindo api breaking news e com dados reais

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,6 +1,6 @@
 import { FaRegHeart, FaRegCommentDots  } from "react-icons/fa";
 
-export default function Card({news}) {
+export default function Card(props) {
     return (
        
             <div className="w-[700px] flex h-auto flex-col py-5 px-10 bg-white mt-4 rounded-md shadow-custom-card">
@@ -8,23 +8,23 @@ export default function Card({news}) {
                 <div className="flex w-full  gap-5">
 
                     <div className="w-[70%]">
-                        <h2 className="font-bold mb-5 mt-8 text-4xl">{news.title}</h2>
-                        <p className="">{news.text}</p>
+                        <h2 className="font-bold mb-5 mt-8 text-4xl">{props.title}</h2>
+                        <p className="">{props.text}</p>
                     </div>
 
                     <div className="w-[30%] flex justify-center">
-                        <img className="w-full h-[250px] object-cover" src={news.image} alt="imagen da Noticia" />
+                        <img className="w-full h-[250px] object-cover" src={props.banner} alt="imagen da Noticia" />
                     </div>
                 </div>
 
                 <div className="flex justify-start items-center mt-2 gap-4 ">
                     <div className="flex items-center gap-1">
                         <FaRegHeart />
-                        <span>{news.likes}</span>
+                        <span>{props.likes}</span>
                     </div>
                     <div className="flex items-center gap-1">
                         <FaRegCommentDots />
-                        <span>{news.comments}</span>
+                        <span>{props.comments}</span>
                     </div>
                 </div>
             </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import Card from "../../components/Cards/Card";
 import Navbar from "../../components/Navbar/Navbar";
 import {news} from "../../Datas.js"
+import { getAllPosts } from "../../services/userServices.js";
 
 export default function(){
+
+    const [news, setNews] = useState([])
+
+    async function findAllNews(){
+        const response = await getAllPosts()
+        setNews(response.data.results)
+    }
+
+    findAllNews()
+
     return (
         <div className="w-full">
             <Navbar />
             <div className=" flex flex-wrap justify-center  px-10 bg-[#F1F1F1] gap-2 ">
-                {news.map((ele, index) =>{
-                    return  <Card key={index} news={ele}/>
+                {news.map((ele) =>{
+                    return  <Card 
+                                key={ele.id} 
+                                title={ele.title}
+                                text={ele.text}
+                                banner={ele.banner}
+                                likes={ele.lenght}
+                                comments={ele.lenght}
+                            />
                 })}
              </div>
         </div>
