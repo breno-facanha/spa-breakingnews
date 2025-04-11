cdf73986784db4b5a92e8469dc8f47e36c5a5159
Breno Façanha <brenofacanha.ti@gmail.com>
2025-04-11 00:22:25 -0300

    colocando api de produção para funcionar

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseUrl = "https://api-breakingnews-cb62.onrender.com"
-const baseUrl = "http://localhost:3100"
+const baseUrl = "https://api-breakingnews-cb62.onrender.com"
+// const baseUrl = "http://localhost:3100"
 
 export function getAllPosts(){
     const response = axios.get(`${baseUrl}/news`)
